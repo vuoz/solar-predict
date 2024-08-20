ab2075fcaba342dc1d2be588426f6ff7771ab59e
zvup <121556153+zvup@users.noreply.github.com>
2024-08-20 23:46:20 +0200

    more work on dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,13 @@
 import os;
 from dataclasses import dataclass;
+from datetime import datetime;
 
 
 
 
 @dataclass
 class SenecExportType():
-    time:str
+    time:datetime
     gridexport: float
     usage: float
     acculevel: float
@@ -14,14 +15,27 @@ class SenecExportType():
     production: float
     accuvolatge: float
     accucurrent:float
-    
-
-    def parse(self,line: str):
-        pass
 
 
 
-    
+ 
+def parse_senec_export_type(line: str)-> tuple[SenecExportType|None,Exception|None]:
+    date_format = "%d.%m.%Y %H:%M:%S"   
+
+    splits = line.split(";")
+    if len(splits) != 7:
+        return (None, Exception(f"Invalid line: {line}"))
+    date_string  = splits[0]
+    time = datetime.strptime(date_string, date_format)
+    gridexport = float(splits[1])
+    usage = float(splits[2])
+    acculevel = float(splits[3])
+    accudischarge = float(splits[4])
+    production = float(splits[5])
+    accuvoltage = float(splits[6])
+    accucurrent = float(splits[7])
+    return (SenecExportType(time,gridexport,usage,acculevel,accudischarge,production,accuvoltage,accucurrent),None)
+   
 class Dataloader():
     def __init__(self, path):
         self.path = path
