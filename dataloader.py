23ae36a9176f4bfc9b70cdff6f5387ed2a8f5567
zvup <121556153+zvup@users.noreply.github.com>
2024-08-21 23:54:06 +0200

    started visualizing data, some improvements are needed for splitting the data by days

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,8 @@
-import os;
+import os; import matplotlib.pyplot as plt;
+from pandas import to_datetime
+import polars as pl;
 from dataclasses import dataclass;
 from datetime import datetime;
-
 
 
 
@@ -46,13 +47,51 @@ class Dataloader():
             csv_files = [] 
             for file in os.listdir(self.path):
                 if file.endswith(".csv"):
-                    csv_files.append(file)
+                    csv_files.append(self.path+"/"+file)
             return csv_files
         
+        def read_csv_and_display_daily_data(file_name : str):
+            df = pl.read_csv(file_name,separator=";")
+            df = df.with_columns(
+                    pl.col(col).str.replace(",", ".").cast(pl.Float64) for col in df.columns[1:] 
+            )
+            
+            df = df.with_columns(
+                pl.col("Uhrzeit").str.to_datetime().alias("Timestamp")
+            )
+
+            df = df.with_columns(
+                pl.col("Timestamp").dt.date().alias("Date")
+            )
+            
+
+            dfs_per_day = [df.filter(pl.col("Date") == date) for date in df.select(pl.col("Date")).unique().to_series()]
+            print(len(dfs_per_day))
+            
+
+            example_date_df = dfs_per_day[0]
+            
+            df_pandas = example_date_df.to_pandas()
+            plt.figure(figsize=(10, 6))
+            plt.scatter(df_pandas['Timestamp'], df_pandas['Stromerzeugung [kW]'], color='blue')
+            plt.xlabel('Time')
+            plt.ylabel('Electricity Production [kW]')
+            plt.title('Electricity Production Over Time')
+            plt.grid(True)
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            plt.show()  
+            
+
+
         csv_files = get_data_files(self.path)
+        read_csv_and_display_daily_data(csv_files[0])
+        
 
         
 
 
 
             
+DataLoader = Dataloader("data")
+DataLoader.load()
